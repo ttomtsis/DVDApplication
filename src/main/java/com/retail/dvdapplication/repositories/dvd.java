450fe20dedc2252d9b93e674d72a2d16f2241326
Tomtsis T <ttomtsis@protonmail.com>
2023-03-27 14:40:41 +0300

    DVD Domain object complete impl

--- src/main/java/com/retail/dvdapplication/repositories/dvd.java
+++ src/main/java/com/retail/dvdapplication/repositories/dvd.java
@@ -4,17 +4,55 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+/*
+* Domain object, represents DVD. Will be inserted to MySQL DB
+* Manipulated by DVDService class
+*/
+
 @Entity
 public class dvd {
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @Id
     @GeneratedValue
+    private Long id;
+    private String name;
+    private String genre;
+    private int reserve;
+
+    protected dvd() {}
+
+    dvd (String name, String genre, int reserve) {
+        this.name = name;
+        this.genre = genre;
+        this.reserve = reserve;
+    }
+
+    // SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setReserve(int reserve) {
+        this.reserve = reserve;
+    }
+
+    // GETTERS
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public int getReserve() {
+        return this.reserve;
     }
 }
