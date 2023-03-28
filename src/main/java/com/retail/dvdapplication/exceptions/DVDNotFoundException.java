6e46eed58c7a9dc6ad5a790427f46d6cff9c62d4
Tomtsis T <ttomtsis@protonmail.com>
2023-03-28 18:46:56 +0300

    Exception can now be called with name, id or name and id. Also implemented advice.

--- src/main/java/com/retail/dvdapplication/exceptions/DVDNotFoundException.java
+++ src/main/java/com/retail/dvdapplication/exceptions/DVDNotFoundException.java
@@ -1,10 +1,14 @@
 package com.retail.dvdapplication.exceptions;
 
-import java.util.function.Supplier;
-
 public class DVDNotFoundException extends RuntimeException {
 
     public DVDNotFoundException(long id) {
-        super("The DVD with ID " + id + "could not be found in the DB");
+        super("The DVD with ID " + id + " could not be found in the Database");
+    }
+    public DVDNotFoundException(String name) {
+        super("The DVD with NAME: " + name + " could not be found in the Database");
+    }
+    public DVDNotFoundException(long id, String name) {
+        super("The DVD with ID: " + id + " and NAME: " + name +" could not be found in the Database");
     }
 }
