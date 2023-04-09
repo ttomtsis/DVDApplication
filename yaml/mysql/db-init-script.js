7bff19795fb5175a4e671e8875375955e3713bc6
Thomas Tomtsis <101430953+ttomtsis@users.noreply.github.com>
2023-04-09 16:41:30 +0300

    Update db-init-script.js

--- yaml/mysql/db-init-script.js
+++ yaml/mysql/db-init-script.js
@@ -3,4 +3,4 @@
 // Possible to get values from environment variables ??
 var data = {host: 'localhost', port:4306, user:'root', password: 'icsd15201'};
 var options = {interactive: false};
-dba.configureInstance(data,options);
+
