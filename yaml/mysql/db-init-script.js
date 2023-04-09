e1906e893d53b28a6704392c492591e6f087945d
Thomas Tomtsis <101430953+ttomtsis@users.noreply.github.com>
2023-04-09 23:24:25 +0300

    Delete db-init-script.js

--- yaml/mysql/db-init-script.js
+++
@@ -1,6 +0,0 @@
-// Used by the MySQL Shell to initialize and prepare a node to become a member of the InnoDB Cluster
-
-// Possible to get values from environment variables ??
-var data = {host: 'localhost', port:4306, user:'root', password: 'icsd15201'};
-var options = {interactive: false};
-dba.configureInstance(data,options);
