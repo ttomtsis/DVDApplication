31a47abc3f6c2da3d51b6ce2a798f07f4fd11933
Tomtsis T <ttomtsis@protonmail.com>
2023-06-28 18:40:25 +0300

    Modified methods to implement pagination

--- src/main/java/com/retail/dvdapplication/model/repository/DVDRepository.java
+++ src/main/java/com/retail/dvdapplication/model/repository/DVDRepository.java
@@ -1,19 +1,24 @@
 package com.retail.dvdapplication.model.repository;
 
 import com.retail.dvdapplication.model.domain.DVD;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /*
 * Provides methods for CRUD operations on DVD objects
-* Implemented by DVDService
+* Used by DVDService
 */
 @Repository
 public interface DVDRepository extends JpaRepository<DVD, Long> {
 
-    // Finds all DVDs that match the name given, is NOT case sensitive
+    // Finds all DVDs that match the name given, is NOT case-sensitive
     // eg: For name=wulf, the following DVDs will be returned, Beowulf, Beowulf 132, wulf 132
-    List<DVD> findByNameContainingIgnoreCase(String name);
+    Page<DVD> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    @Override
+    @NonNull
+    Page<DVD> findAll(@NonNull Pageable pageable);
+
 }
