1645615f69ef3c63e4a88a651c95f7f3f0f1e3ac
Tomtsis T <ttomtsis@protonmail.com>
2023-06-29 13:15:24 +0300

    Controller now returns resource representations

--- src/main/java/com/retail/dvdapplication/controller/DVDController.java
+++ src/main/java/com/retail/dvdapplication/controller/DVDController.java
@@ -1,12 +1,15 @@
 package com.retail.dvdapplication.controller;
 
 import com.retail.dvdapplication.model.domain.DVD;
+import com.retail.dvdapplication.model.representation.DVDModel;
+import com.retail.dvdapplication.model.representation.DVDModelAssembler;
 import com.retail.dvdapplication.model.service.DVDService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,42 +25,51 @@ public class DVDController {
     private final DVDService service; // Service class
     private static final Logger log = LoggerFactory.getLogger("DVD Controller");
 
-    public DVDController(DVDService service) {
+    private final DVDModelAssembler assembler;
+    public DVDController(DVDService service, DVDModelAssembler assembler) {
         this.service = service;
+        this.assembler = assembler;
     }
 
     @PostMapping("")
-    public ResponseEntity<DVD> createDVD(@RequestBody DVD new_dvd) {
-        log.info("New DVD Creation Request: " + new_dvd.toString());
-        return new ResponseEntity<>(service.createDVD(new_dvd), HttpStatus.CREATED);
+    public ResponseEntity<DVDModel> createDVD(@RequestBody DVD newDVD) {
+        log.info("New DVD Creation Request: " + newDVD.toString());
+        DVD createdDVD = service.createDVD(newDVD);
+        return new ResponseEntity<>(assembler.toModel(createdDVD), HttpStatus.CREATED);
     }
 
     @GetMapping("")
-    public ResponseEntity<Page<DVD>> searchAllDVDs(@RequestParam(defaultValue = "0") int page,
+    public ResponseEntity<PagedModel<DVDModel>> searchAllDVDs(@RequestParam(defaultValue = "0") int page,
                                                    @RequestParam(defaultValue = "10") int size) {
         log.info("New DVD Search Request: Search All");
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(service.searchAllDVDs(pageable), HttpStatus.OK);
+        Page<DVD> dvdPage = service.searchAllDVDs(pageable);
+        PagedModel<DVDModel> myModel = assembler.createPagedModelForSearchAll(dvdPage);
+        return new ResponseEntity<>(myModel, HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Page<DVD>> searchDVDByName(@RequestParam String name, @RequestParam(defaultValue = "0") int page,
+    public ResponseEntity<PagedModel<DVDModel>> searchDVDByName(@RequestParam String name, @RequestParam(defaultValue = "0") int page,
                                                      @RequestParam(defaultValue = "10") int size) {
         log.info("New DVD Search Request: Search name - " + name);
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(service.searchDVDByName(name, pageable), HttpStatus.OK);
+        Page<DVD> dvdPage = service.searchDVDByName(name, pageable);
+        PagedModel<DVDModel> myModel = assembler.createPagedModelForSearchByName(dvdPage, name);
+        return new ResponseEntity<>(myModel, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DVD> searchDVDByID(@PathVariable long id) {
+    public ResponseEntity<DVDModel> searchDVDByID(@PathVariable long id) {
         log.info("New DVD Search Request: Search id - " + id);
-        return new ResponseEntity<>(service.searchDVDByID(id), HttpStatus.OK);
+        DVD requestedDVD = service.searchDVDByID(id);
+        return new ResponseEntity<>(assembler.toModel(requestedDVD), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<DVD> updateDVDByID(@PathVariable long id, @RequestBody DVD newDVD) {
+    public ResponseEntity<DVDModel> updateDVDByID(@PathVariable long id, @RequestBody DVD newDVD) {
             log.info("New DVD Update Request: Update DVD - " + id);
-            return new ResponseEntity<>(service.updateDVDByID(id, newDVD),HttpStatus.OK);
+            DVD updatedDVD = service.updateDVDByID(id, newDVD);
+            return new ResponseEntity<>(assembler.toModel(updatedDVD),HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
